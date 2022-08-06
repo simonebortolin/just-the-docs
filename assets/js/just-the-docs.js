36b33a71f01f3f73b6c3b87bfa98d0e148f40282
Simone Bortolin <info@simonebortolin.it>
2022-08-06 21:54:51 +0200

    add copy button

--- assets/js/just-the-docs.js
+++ assets/js/just-the-docs.js
@@ -466,6 +466,41 @@ jtd.onReady(function(){
   {%- endif %}
 });
 
+jtd.onReady(function(){
+  // Copy button on code
+
+  var codeBlocks = document.querySelectorAll('pre.highlight');
+
+  var svgCopied = "<svg xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='currentColor' class='bi bi-clipboard-check-fill' viewBox='0 0 16 16'><path d='M6.5 0A1.5 1.5 0 0 0 5 1.5v1A1.5 1.5 0 0 0 6.5 4h3A1.5 1.5 0 0 0 11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3Zm3 1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-3a.5.5 0 0 1-.5-.5v-1a.5.5 0 0 1 .5-.5h3Z'></path><path d='M4 1.5H3a2 2 0 0 0-2 2V14a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V3.5a2 2 0 0 0-2-2h-1v1A2.5 2.5 0 0 1 9.5 5h-3A2.5 2.5 0 0 1 4 2.5v-1Zm6.854 7.354-3 3a.5.5 0 0 1-.708 0l-1.5-1.5a.5.5 0 0 1 .708-.708L7.5 10.793l2.646-2.647a.5.5 0 0 1 .708.708Z'></path></svg>";
+  var svgCopy = "<svg xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='currentColor' class='bi bi-clipboard' viewBox='0 0 16 16'><path d='M4 1.5H3a2 2 0 0 0-2 2V14a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V3.5a2 2 0 0 0-2-2h-1v1h1a1 1 0 0 1 1 1V14a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V3.5a1 1 0 0 1 1-1h1v-1z'/><path d='M9.5 1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-3a.5.5 0 0 1-.5-.5v-1a.5.5 0 0 1 .5-.5h3zm-3-1A1.5 1.5 0 0 0 5 1.5v1A1.5 1.5 0 0 0 6.5 4h3A1.5 1.5 0 0 0 11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3z'/></svg>";
+
+  [...codeBlocks].forEach(codeBlock => {
+    var copyButton = document.createElement('button');
+    copyButton.type = 'button';
+    copyButton.className = 'icon-btn';
+    copyButton.ariaLabel = 'Copy code to clipboard';
+    copyButton.innerHTML = svgCopy;
+    codeBlock.append(copyButton);
+
+    copyButton.addEventListener('click', function () {
+      var code = codeBlock.querySelector('code').innerText.trim();
+      window.navigator.clipboard.writeText(code);
+
+      copyButton.innerHTML = svgCopied;
+      var fourSeconds = 4000;
+
+      setTimeout(function () {
+        copyButton.innerHTML = svgCopy;
+      }, fourSeconds);
+    });
+  });
+
+});
+
+
+
 })(window.jtd = window.jtd || {});
+
+
 
 {% include js/custom.js %}
